5a2e751f530ff76f861d94c10c7d59f6a0763d10
riatmax <riatmax@gmail.com>
2025-05-13 14:46:14 -0400

    added character flipping, camera left/right movement

---
+++ Assets/Scripts/Chunk/Camera/CameraContainer.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraContainer : MonoBehaviour
+{
+    public CinemachineCamera[] Cameras;
+}
--- Assets/Scripts/Chunk/Chunk.cs
+++ Assets/Scripts/Chunk/Chunk.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 
 public class Chunk : MonoBehaviour
 {
     private Rigidbody2D rb;
     private PlayerControls playerControls;
     private Vector2 movementInput;
+    private SpriteRenderer sprRend;
 
+    private CinemachineCamera[] camContainer;
     private bool isGrounded;
     private bool jumpHeld;
+    private bool facingLeft = false;
 
     [Header("Chunk Stats")]
     [SerializeField] private float jumpForce;
@@ -19,6 +23,8 @@ public class Chunk : MonoBehaviour
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        sprRend = GetComponentInChildren<SpriteRenderer>();
+        camContainer = GetComponentInChildren<CameraContainer>().Cameras;
     }
     private void FixedUpdate()
     {
@@ -33,6 +39,18 @@ public class Chunk : MonoBehaviour
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDist, LayerMask.GetMask("Ground"));
         Debug.DrawLine(transform.position, transform.position - new Vector3(0, groundCheckDist, 0), Color.green);
+        if (facingLeft)
+        {
+            sprRend.flipX = true;
+            camContainer[0].Priority = 0;
+            camContainer[1].Priority = 1;
+        }
+        else
+        {
+            sprRend.flipX = false;
+            camContainer[1].Priority = 0;
+            camContainer[0].Priority = 1;
+        }
     }
     public void Jump(InputAction.CallbackContext ctx)
     {
@@ -48,6 +66,14 @@ public class Chunk : MonoBehaviour
     public void Move(InputAction.CallbackContext ctx)
     {
         movementInput = ctx.ReadValue<Vector2>();
+        if (ctx.ReadValue<Vector2>().x > 0)
+        {
+            facingLeft = false;
+        } 
+        if (ctx.ReadValue<Vector2>().x < 0)
+        {
+            facingLeft = true;
+        }
     }
 
     private void Jump()
